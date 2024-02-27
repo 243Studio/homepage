924002dea16cf921ba28574441143b9dd29de1f6
LeGrandMAG <magmukendi0@gmail.com>
2024-02-27 13:35:38 -0500

    updated contact infos

--- src/Page/Contact.jsx
+++ src/Page/Contact.jsx
@@ -13,12 +13,8 @@ export default function Contact() {
     <div className='contact-container'>
       <h2 id="contact-title">Contactez-nous</h2>
       <ContactItem
-      href='mailto:creative243.studio@gmail.com'
-      text="creative243.studio@gmail.com"
-      />
-      <ContactItem
-      href='https://wa.me/15145898716'
-      text="+1-514-589-8716"
+      href='mailto:info@243studio.me'
+      text="info@243studio.me"
       />
     </div>
   )
